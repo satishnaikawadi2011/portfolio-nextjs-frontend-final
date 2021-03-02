af8196b0479807a11fb3e435ff0750d368659599
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2021-03-02 18:30:21 +0530

    Added BlogCard Component.

---
+++ components/blog-card/BlogCard.tsx
@@ -0,0 +1,43 @@
+import { motion } from 'framer-motion';
+import React from 'react';
+import { cardVariant } from '../../utils/variants';
+import styles from './blog-card.module.css';
+import Rosa from 'react-on-scroll-animation';
+import Link from 'next/link';
+import Tag from '../tag/Tag';
+
+interface BlogCardProps {
+	image: string;
+	title: string;
+	tags: string[];
+	createdAt: string;
+	id: string;
+	readingTime: string;
+}
+
+const BlogCard: React.FC<BlogCardProps> = ({ image, title, tags, createdAt, children, id, readingTime }) => {
+	return (
+		<motion.div variants={cardVariant} initial="hidden" animate="visible" className={styles.blog__card}>
+			<Rosa animation="flip-left">
+				<div className={styles.blog__img__container}>
+					<img src={image} alt={title} className={styles.blog__image} />
+					<span className={styles.blog__createdAt__badge}>{createdAt}</span>
+				</div>
+				<div className={styles.blog__card__body}>
+					<Link href={`/posts/${id}`}>
+						<h2 className={styles.blog__title}>{title}</h2>
+					</Link>
+					<p className={styles.blog__description}>{children}</p>
+					<div className={styles.blog__tags__container}>
+						{tags.slice(0, 3).map((tagName) => {
+							return <Tag tagName={tagName} />;
+						})}
+					</div>
+					<strong className={styles.reading__time}>&bull; {readingTime}</strong>
+				</div>
+			</Rosa>
+		</motion.div>
+	);
+};
+
+export default BlogCard;
