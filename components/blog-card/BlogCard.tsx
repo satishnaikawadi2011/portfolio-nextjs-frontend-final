932434c6365f170b72a7548106629d0361a48957
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2021-03-02 19:45:28 +0530

    Updated BlogCard component.

--- components/blog-card/BlogCard.tsx
+++ components/blog-card/BlogCard.tsx
@@ -5,32 +5,30 @@ import styles from './blog-card.module.css';
 import Rosa from 'react-on-scroll-animation';
 import Link from 'next/link';
 import Tag from '../tag/Tag';
+import { Post } from '../../models/Post';
 
 interface BlogCardProps {
-	image: string;
-	title: string;
-	tags: string[];
-	createdAt: string;
-	id: string;
-	readingTime: string;
+	post: Post;
 }
 
-const BlogCard: React.FC<BlogCardProps> = ({ image, title, tags, createdAt, children, id, readingTime }) => {
+const BlogCard: React.FC<BlogCardProps> = ({
+	post: { description, cover, title, tags, published_at, id, readingTime }
+}) => {
 	return (
 		<motion.div variants={cardVariant} initial="hidden" animate="visible" className={styles.blog__card}>
 			<Rosa animation="flip-left">
 				<div className={styles.blog__img__container}>
-					<img src={image} alt={title} className={styles.blog__image} />
-					<span className={styles.blog__createdAt__badge}>{createdAt}</span>
+					<img src={cover} alt={title} className={styles.blog__image} />
+					<span className={styles.blog__createdAt__badge}>{published_at}</span>
 				</div>
 				<div className={styles.blog__card__body}>
 					<Link href={`/posts/${id}`}>
 						<h2 className={styles.blog__title}>{title}</h2>
 					</Link>
-					<p className={styles.blog__description}>{children}</p>
+					<p className={styles.blog__description}>{description}</p>
 					<div className={styles.blog__tags__container}>
-						{tags.slice(0, 3).map((tagName) => {
-							return <Tag tagName={tagName} />;
+						{tags.slice(0, 3).map((tag) => {
+							return <Tag tagName={tag.name} />;
 						})}
 					</div>
 					<strong className={styles.reading__time}>&bull; {readingTime}</strong>
