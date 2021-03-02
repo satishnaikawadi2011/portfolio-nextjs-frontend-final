1a0fca303ffd11862b8fab1170c739ae88f10758
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2021-03-02 17:07:39 +0530

    Added SocialIcons component.

---
+++ components/social-icons/SocialIcons.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import styles from './social-icons.module.css';
+import { FaFacebookF, FaInstagram, FaLinkedinIn, FaTwitter } from 'react-icons/fa';
+import Link from 'next/link';
+
+interface SocialIconsProps {
+	color?: string;
+}
+
+const Socialicons: React.FC<SocialIconsProps> = ({ color = 'var(--primaryText)' }) => {
+	return (
+		<div className={styles.social__icons}>
+			<Link href="https://www.facebook.com/satishananda.naikawadi.3">
+				<FaFacebookF
+					className={styles.social__icon}
+					style={{ fill: color, border: `3px solid ${color}` }}
+					fill={color}
+					color={color}
+				/>
+			</Link>
+			<Link href="https://www.instagram.com/satish_011e/">
+				<FaInstagram
+					className={styles.social__icon}
+					style={{ fill: color, border: `3px solid ${color}` }}
+					fill={color}
+					color={color}
+				/>
+			</Link>
+			<Link href="/">
+				<FaLinkedinIn
+					className={styles.social__icon}
+					style={{ fill: color, border: `3px solid ${color}` }}
+					fill={color}
+					color={color}
+				/>
+			</Link>
+			<Link href="/">
+				<FaTwitter
+					className={styles.social__icon}
+					style={{ fill: color, border: `3px solid ${color}` }}
+					fill={color}
+					color={color}
+				/>
+			</Link>
+		</div>
+	);
+};
+
+export default Socialicons;
