985bbbc71517c101276433ae552836b43cd3022f
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2021-03-07 17:22:46 +0530

    Added some common meta tags in _document.tsx..

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,7 +9,13 @@ class MyDocument extends Document {
 	render() {
 		return (
 			<Html data-theme="dark" lang="en">
-				<Head />
+				<Head>
+					<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0" />
+					<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+					<meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+					<meta name="HandheldFriendly" content="True" />
+					<meta name="MobileOptimized" content="320" />
+				</Head>
 				<body>
 					<Main />
 					<NextScript />
