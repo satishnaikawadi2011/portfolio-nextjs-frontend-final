6b46a28693ecc708eb2ee0881eadf4319aa40261
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2021-03-08 15:39:13 +0530

    Converted to PWA with next-pwa package.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,6 +15,9 @@ class MyDocument extends Document {
 					<meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
 					<meta name="HandheldFriendly" content="True" />
 					<meta name="MobileOptimized" content="320" />
+					<link rel="manifest" href="/manifest.json" />
+					<link rel="apple-touch-icon" href="/images/logo.png" />
+					<meta name="theme-color" content="#2196f3" />
 				</Head>
 				<body>
 					<Main />
